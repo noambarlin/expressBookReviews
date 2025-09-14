6c0697bd7913a412d0edb209936be705b292e1d7
Your Name <you@example.com>
2025-09-14 05:15:01 -0400

    5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,11 +28,34 @@ public_users.get('/',function (req, res) {
   res.send(JSON.stringify(books, null,4));
 });
 
+public_users.get('/', (req, res) => {
+    new Promise((resolve) => resolve(books))
+      .then((data) => res.status(200).json(data))
+      .catch((err) =>
+        res.status(500).json({ message: 'Error fetching books', error: err?.message })
+      );
+  });
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
   res.send(books[isbn]);
+ });
+
+
+ public_users.get('/isbn/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+    new Promise((resolve, reject) => {
+        const book = books[isbn];
+        if (book) {
+            resolve(book);
+        } else {
+            reject(new Error("book not found"));
+        }
+    })
+    .then((book)=> res.status(200).json(book))
+    .catch((err)=> res.status(404).json({message: err.message}));
  });
   
 // Get book details based on author
@@ -43,12 +66,41 @@ public_users.get('/author/:author',function (req, res) {
   res.send(result);
 });
 
+
+public_users.get('/author/:author', (req,res)=> {
+    const author = req.params.author;
+    new Promise((resolve, reject)=>{
+        const matching = Object.values(books).filter((book) => book.author === author);
+        if (matching.length > 0) {
+            resolve(matching);
+        } else {
+            reject(new Error("no books found for this author"));
+        }
+    })
+    .then((data)=> res.status(200).json(data))
+    .catch((err)=> res.status(404).json({message: err.message}));
+});
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
   const title = req.params.title;
   const result = Object.values(books).filter((book) => book.title === title);
   res.send(result);
+});
+
+public_users.get('title/:title', (req,res)=> {
+    const title = req.params.title;
+    new Promise((resolve, reject)=> {
+        const matching = Object.values(books).filter((book)=> book.title === title);
+        if (matching.length > 0) {
+            resolve(matching);
+        } else {
+            reject(new Error("no cooks are found for this title"));
+        }
+    })
+    .then((data)=> res.status(200).json(data))
+    .catch((err)=> res.status(404).json({message: err.message}));
 });
 
 //  Get book review
